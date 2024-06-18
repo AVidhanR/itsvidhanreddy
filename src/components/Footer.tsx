2bed2704dec67865a748f8fd20e87f788d6c3c2e
AVidhanR <itsvidhanreddy@gmail.com>
2024-06-18 11:26:18 +0530

    Update Footer component to fix author name display

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="m-6 flex items-center justify-center">
-      &copy;A Vidhan Reddy
+      @A Vidhan Reddy
     </footer>
   );
 }
