6a921a1440eb1323c380d1ca2f033f6a3ab10f99
AVidhanR <itsvidhanreddy@gmail.com>
2024-10-17 15:16:33 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="m-6 flex items-center justify-center">
-      &copy;A Vidhan Reddy
+      &copy;{new Date().getFullYear()} A Vidhan Reddy
     </footer>
   );
 }
