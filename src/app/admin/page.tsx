132d5fef76102a1b31b2e9f320990693883fba7c
AVidhanR <itsvidhanreddy@gmail.com>
2024-10-18 20:56:12 +0530

    feat: Add AdminDashboard component

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AdminDashboard() {
+  return (
+    <div className="text-3xl text-white flex font-mono justify-center text-center mt-28 mx-1">
+      Hello! This is still experimental. <br /> Please check back later! (-_-)
+    </div>
+  );
+}
