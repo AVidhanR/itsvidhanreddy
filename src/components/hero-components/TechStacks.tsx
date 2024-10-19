ea3c94cbb7e3e0c969c3f519a1f1103fbafae0aa
AVidhanR <itsvidhanreddy@gmail.com>
2024-10-19 13:40:02 +0530

    Update TechStacks.tsx
    Nothing much!

--- src/components/hero-components/TechStacks.tsx
+++ src/components/hero-components/TechStacks.tsx
@@ -2,7 +2,11 @@ import React from "react";
 
 // trying out es6 syntax
 const TechStacks = () => {
-  return <div>My Tech Stacks</div>;
+  return (
+    <div className="p-4 font-bold cursor-pointer text-start text-2xl hover:text-blue-500 hover:transition-colors">
+      Tech Stacks
+    </div>
+  );
 };
 
 export default TechStacks;
