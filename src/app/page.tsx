f6c0b5ba4845d6fba6c744bfd6d5c48faafe213a
AVidhanR <itsvidhanreddy@gmail.com>
2024-10-19 13:29:00 +0530

    Update page.tsx
    Added the tech stacks to the hero section page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/hero-components/Hero";
+import TechStacks from "@/components/hero-components/TechStacks";
 
 export default function Home() {
   return (
     <main className="">
       <Hero />
+      <TechStacks />
     </main>
   );
 }
